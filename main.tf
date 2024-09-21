17dd4ffcf214941746250cc7e272e2de49caff75
Phyllis Likimani <likiphyllis@yahoo.com>
2024-09-21 16:33:23 -0500

    vcs workflow

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-# resource "aws_vpc" "test_vpc" {
-#   cidr_block = "10.0.0.0/16"
-# }
-# resource "google_storage_bucket" "my_storage_bucket" {
-#   name          = "likimani-no-public-access-bucket"
-#   location      = "US"
-#   force_destroy = true
+resource "aws_vpc" "test_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+resource "google_storage_bucket" "my_storage_bucket" {
+  name          = "likimani-no-public-access-bucket"
+  location      = "US"
+  force_destroy = true
 
-#   public_access_prevention = "enforced"
-# }
+  public_access_prevention = "enforced"
+}
