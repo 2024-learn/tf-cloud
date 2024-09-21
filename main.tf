3bb3324bb2474d5d81032ab5b02a2659a852e5c8
phyllis likimani <79490445+phyllisn-landmark@users.noreply.github.com>
2024-09-21 16:39:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,1 @@
-resource "aws_vpc" "test_vpc" {
-  cidr_block = "10.0.0.0/16"
-}
-resource "google_storage_bucket" "my_storage_bucket" {
-  name          = "likimani-no-public-access-bucket"
-  location      = "US"
-  force_destroy = true
 
-  public_access_prevention = "enforced"
-}
