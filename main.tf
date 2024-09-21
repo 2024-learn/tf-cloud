3e652ac1c37d9a265a137e2a57bd1112ac423d2c
Phyllis Likimani <likiphyllis@yahoo.com>
2024-09-21 17:38:39 -0500

    prod-changes

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 # resource "aws_vpc" "test_vpc" {
 #   cidr_block = "10.0.0.0/16"
 # }
+
 # resource "google_storage_bucket" "my_storage_bucket" {
 #   name          = "likimani-staging-no-public-access"
 #   location      = "US"
@@ -8,6 +9,7 @@
 
 #   public_access_prevention = "enforced"
 # }
+
 # resource "google_storage_bucket" "staging_storage_bucket" {
 #   name          = "likimani-staging-public-access"
 #   location      = "US"
@@ -15,6 +17,7 @@
 
 #   public_access_prevention = "enforced"
 # }
+
 # resource "google_storage_bucket" "public_storage_bucket" {
 #   name          = "likimani-public-access-bucket"
 #   location      = "US"
