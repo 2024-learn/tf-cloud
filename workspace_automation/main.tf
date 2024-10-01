18234b8e1e14881f0510c21d82e1390c4a9e33ee
Phyllis Likimani <likiphyllis@yahoo.com>
2024-10-01 07:13:32 -0500

    merge repos and tfe provider added

---
+++ workspace_automation/main.tf
@@ -0,0 +1,22 @@
+provider "tfe" {}
+
+variable "workspace_name" {
+  default = "likimani-dev-workspace"
+}
+
+variable "organization" {
+  default = "likimani-tf-cloud"
+}
+
+data "tfe_workspace" "tfe_workspace" {
+  name = var.workspace_name
+  organization = var.organization
+}
+
+output "workspace_id" {
+  value = data.tfe_workspace.tfe_workspace.id
+}
+
+output "workspace_terraform_version" {
+  value = data.tfe_workspace.tfe_workspace.terraform_version
+}
