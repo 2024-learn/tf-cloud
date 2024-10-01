7bca5b01efab35f3bcc0d08fc0fad51e96341870
Phyllis Likimani <likiphyllis@yahoo.com>
2024-10-01 10:38:36 -0500

    dev

--- workspace_automation/main.tf
+++ workspace_automation/main.tf
@@ -17,15 +17,15 @@ data "tfe_organization" "tfcloud_org" {
 output "tfe_project_id" {
   value = data.tfe_organization.tfcloud_org.default_project_id
 }
-# resource "tfe_workspace" "new_dev_workspace" {
-#   name         = var.new_dev_workspace
-#   organization = var.organization
-#   project_id = data.tfe_project.tf_cloud_project.id
-# }
+resource "tfe_workspace" "new_dev_workspace" {
+  name         = var.new_dev_workspace
+  organization = var.organization
+  project_id = data.tfe_organization.tfcloud_org.default_project_id
+}
 
-# output "new_dev_workspace_id" {
-#   value = tfe_workspace.new_dev_workspace.id
-# }
+output "new_dev_workspace_id" {
+  value = tfe_workspace.new_dev_workspace.id
+}
 
 # output "new_dev_workspace_tf_version" {
 #   value = tfe_workspace.new_dev_workspace.terraform_version
